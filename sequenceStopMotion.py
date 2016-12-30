626db265f00caae2232aedd2c954357b18fa9c77
JulienLegrand <JulienLegrand@users.noreply.github.com>
2016-12-30 21:32:12 +0000

    NEW Stop motion working but not perfect

--- sequenceStopMotion.py
+++ sequenceStopMotion.py
@@ -54,18 +54,47 @@ def TakePictures():
     return photoFile
 
 def Composite(photoFile):
-	print "Create video"
-	#os.popen don't work for this, don't know why
-	p = sub.Popen("sudo mencoder mf://stopmotion-temp/*.jpg -mf w=1920:h=1080:fps=25:type=jpg:fps=1 -nosound -ovc lavc -o stopmotion-temp/" + photoFile + ".mp4", stdout=sub.PIPE, stderr=sub.PIPE, shell=True)
-	p.wait()
-	
-	#Add music
-	p = sub.Popen("sudo avconv -i stopmotion-temp/" + photoFile + ".mp4 -i music.mp3 -c copy -map 0:v -map 1:a -shortest stopmotion-composites/" + photoFile + ".mp4 -y", stdout=sub.PIPE, stderr=sub.PIPE, shell=True)
-	p.wait()
+    print "Prepare images"
 
-	#Play
-	pygameEngine.Fill(pygameEngine.BLACK_COLOR)
-	os.popen("omxplayer stopmotion-composites/" + photoFile + ".mp4")
+    i=1
+    while(i<=NB_PHOTOS):
+        photoFilePathDest = "stopmotion-temp/" + photoFile + "-" + str(i).zfill(2) + ".jpg"
+
+        #Create additionnal images with imagemagick morph
+        if(i>1):
+            p = sub.Popen("sudo convert stopmotion-temp/" + photoFile + "-" + str(i-1).zfill(2) + ".jpg stopmotion-temp/" + photoFile + "-" + str(i).zfill(2) + ".jpg -delay 100 -morph 10 stopmotion-temp/" + photoFile + "-" + str(i-1).zfill(2) + "-99-%03d.jpg", stdout=sub.PIPE, stderr=sub.PIPE, shell=True)
+            p.wait()
+
+        #Duplicate the photo for the final video
+        j=1
+        while(j<=20):
+            copyfile(photoFilePathDest, "stopmotion-temp/" + photoFile + "-" + str(i).zfill(2) + "-" + str(j).zfill(2) + ".jpg")
+            j+=1
+
+        i+=1
+
+    #Realign numbers
+    i=1
+    filelist = [ f for f in os.listdir("stopmotion-temp") ]
+    filelist.sort()
+    for f in filelist:
+        os.rename("stopmotion-temp/" + f, "stopmotion-temp/" + photoFile + "-" + str(i).zfill(3) + ".jpg")
+        i+=1
+
+    print "Encode video"
+    #os.popen don't work for this, don't know why
+    p = sub.Popen("sudo mencoder mf://stopmotion-temp/*.jpg -mf w=1920:h=1080:fps=20:type=jpg -nosound -ovc x264 -x264encopts bitrate=2000 -o stopmotion-temp/" + photoFile + ".mp4", stdout=sub.PIPE, stderr=sub.PIPE, shell=True)
+    p.wait()
+
+    print "Add music"
+    #Add music
+    p = sub.Popen("sudo avconv -i stopmotion-temp/" + photoFile + ".mp4 -i music.mp3 -c copy -map 0:v -map 1:a -shortest stopmotion-composites/" + photoFile + ".mp4 -y", stdout=sub.PIPE, stderr=sub.PIPE, shell=True)
+    p.wait()
+
+    print "Play"
+    #Play
+    pygameEngine.Fill(pygameEngine.BLACK_COLOR)
+    os.popen("omxplayer stopmotion-composites/" + photoFile + ".mp4 -o local --vol -4000")
 
 def Clear():
 	#Clear temp folder
@@ -74,15 +103,15 @@ def Clear():
 		os.remove("stopmotion-temp/" + f)
 	
 def Start():
-	try:
-		print "Stop-Motion Start"
-		livePreview.Start()
-		Init()
-		photoFile = TakePictures()
-		Composite(photoFile)
-		
-		#Clear()
-		print "Stop-Motion End"
-	except Exception, e:
-		print "ERREUR : Stop-Motion : " + e.message
-		pygameEngine.ShowError()
+    try:
+        print "Stop-Motion Start"
+        livePreview.Start()
+        Init()
+        photoFile = TakePictures()
+        Composite(photoFile)
+
+        #Clear()
+        print "Stop-Motion End"
+    except Exception, e:
+        print "ERREUR : Stop-Motion : " + e.message
+        pygameEngine.ShowError()
